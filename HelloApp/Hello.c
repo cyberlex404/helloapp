32a06823c0fae3292f4997fc16c6ec0ce94237e4
Lex Misiuro <cyberlex404@yandex.com>
2018-09-23 15:04:38 +0300

    z9

--- HelloApp/Hello.c
+++ HelloApp/Hello.c
@@ -3,7 +3,30 @@
 #include "simple.h"
 
 void main()                  
-{                               
-	doSomething(); // use external file
+{          
+	int v = 3000;
+	int a = 130, b = 160;
+	int flag = 0;
+	int ca = 0, cb =0;
+
+	while (v > 0) {
+		v = v - a;
+		ca++;
+		if (v%b == 0) {
+			cb = v / b;
+			flag = 1;
+			break;
+		}
+	}
+
+	if (flag) {
+		printf("It's okay (a *%d) + (b * %d) == 3000", ca, cb);
+	}
+	else{
+		printf("Big problem!");
+	}
+	
+	//doSomething(); // use external file
+
 	_getch();
 }                               
--- HelloApp/simple.c
+++ HelloApp/simple.c
@@ -1,6 +1,17 @@
 #include "simple.h"
 
 void doSomething() {
-	printf("It works!");
+
+	if (3000 % 160 == 0) {
+		printf("It works! true");
+	}
+
+	if (1600 % 160 == 0) {
+		printf("It works! true 160");
+	}
+
+
+
+	
 	_getch();
 }
